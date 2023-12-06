cfe0c012b4a164ed8e9332702fc77eebf002202b
chemitaro <iwasi_44@hotmail.com>
2023-12-06 14:49:58 +0900

    Refactor collect_files_recursively to get_all_js_paths

--- apps/lib/collect_files_recursively.py
+++ apps/lib/collect_files_recursively.py
@@ -1,7 +1,11 @@
 import os
 
 
-def collect_files_recursively(root_path: str, extensions: tuple[str, ...], ignore_dirs: list[str]) -> list[str]:
+def get_all_js_paths(
+    root_path: str,
+    ignore_dirs: list[str],
+    extensions: tuple[str, ...] = ('.ts', '.tsx', '.js', '.jsx'),
+) -> list[str]:
     """指定したディレクトリ以下のファイルを再帰的に検索する
 
     Args:
@@ -25,10 +29,9 @@ def collect_files_recursively(root_path: str, extensions: tuple[str, ...], ignor
 directory_to_search = "/Users/iwasawayuuta/workspace/product/taikyohiyou_project/taikyohiyou_webui"
 ignore_paths = ["src", "node_modules"]
 
-extensions = ('.ts', '.tsx', '.js', '.jsx')
 
 # ファイルのパスを検索
-file_paths = collect_files_recursively(directory_to_search, extensions, ignore_paths)
+file_paths = get_all_js_paths(directory_to_search, ignore_paths)
 
 # 結果の配列を表示
 print(file_paths)
