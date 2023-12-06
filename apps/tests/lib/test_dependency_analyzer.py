d11764d5175531116152914c4b0e68b63c4f0d30
chemitaro <iwasi_44@hotmail.com>
2023-12-06 14:45:40 +0900

    Add test for getting all file paths in a directory

---
+++ apps/tests/lib/test_dependency_analyzer.py
@@ -0,0 +1,14 @@
+import os
+from apps.lib.dependency_analyzer.main import get_all_file_paths
+
+
+class TestGetAllFilePaths:
+    # テスト用のファイルとディレクトリとして、mock 以下を使用
+    root_path = os.path.join(os.path.dirname(__file__), 'mock')
+
+    def test_get_all_file_paths(self):
+        # テスト用のディレクトリ以下のファイルパスを取得
+        file_paths = get_all_file_paths(self.root_path)
+
+        # テスト用のディレクトリ以下のファイルパスが取得できていることを確認
+        assert len(file_paths) == 16
