f7084f60a2fb9f698f1ab419c3ae89363fa71704
chemitaro <iwasi_44@hotmail.com>
2023-12-07 11:03:34 +0900

    Update imports in ts_mock_a_1.ts, ts_mock_a_2.ts, ts_mock_b_1.ts, ts_mock_b_2.ts

--- tests/mock/ts_mock/ts_mock_a/ts_mock_a_1.ts
+++ tests/mock/ts_mock/ts_mock_a/ts_mock_a_1.ts
@@ -1,4 +1,6 @@
-import { ts_mock_a_a_1 } from "./ts_mock_a_a/ts_mock_a_a_1";
-import { ts_mock_a_b_1 } from "./ts_mock_a_b/ts_mock_a_b_1";
+import { ts_mock_a_a_1 } from "@/ts_mock/ts_mock_a/ts_mock_a_a/ts_mock_a_a_1";
+import { ts_mock_a_b_1 } from "@/ts_mock/ts_mock_a/ts_mock_a_b/ts_mock_a_b_1";
 
 export const ts_mock_a_1 = 'ts_mock_a_1';
+console.log(ts_mock_a_a_1);
+console.log(ts_mock_a_b_1);
