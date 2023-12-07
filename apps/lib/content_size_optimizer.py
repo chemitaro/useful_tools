4a7c0d5e3486f7534a81d21049e2b8ad8053eb74
chemitaro <iwasi_44@hotmail.com>
2023-12-07 15:36:33 +0900

    Refactor content size optimizer to calculate total token and character counts

--- apps/lib/content_size_optimizer.py
+++ apps/lib/content_size_optimizer.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+
 from apps.lib.utils import count_tokens
 
 
@@ -10,29 +11,37 @@ class CalcSizedContent:
 
 
 class ContentSizeOptimizer:
+    contents: list[str]
     max_token: int
     max_char: int
+    calc_sized_contents: list[CalcSizedContent] = []
 
     def __init__(
         self,
+        contents: list[str],
         max_token: int = 120_000,
         max_char: int = 9_999_999
     ):
+        self.contents = contents
         self.max_token = max_token
         self.max_char = max_char
+        self.calc_size_contents()
 
-    def optimize(self, contents: list[str]) -> list[str]:
+    def optimize_contents(self) -> list[str]:
         """コンテンツのサイズを最大トークン数と最大文字数を超えないように結合したり分割したりする"""
-        # 文字数とトークン数を計算して辞書型まとめてリストに格納する
-        calc_sized_contents: list[CalcSizedContent] = [self.calc_size(content) for content in contents]
-
         # 文字数とトークン数の合計が最大文字数と最大トークン数を超えないようにコンテンツを結合する
-        concat_contents = self.concat_contents(calc_sized_contents)
+        concat_contents = self.concat_contents()
 
         return concat_contents
 
+    def calc_size_contents(self) -> None:
+        self.calc_sized_contents = []
+        for content in self.contents:
+            calc_sized_content: CalcSizedContent = self.calc_size_content(content)
+            self.calc_sized_contents.append(calc_sized_content)
+
     # 文字数とトークン数を計算して辞書型にして返す
-    def calc_size(self, content: str) -> CalcSizedContent:
+    def calc_size_content(self, content: str) -> CalcSizedContent:
         token_size = count_tokens(content)
         char_size = len(content)
         if self.max_token < token_size or self.max_char < char_size:
@@ -46,14 +55,14 @@ class ContentSizeOptimizer:
         return calc_sized_content
 
     # 文字数とトークン数の合計が最大文字数と最大トークン数を超えないようにコンテンツを結合する
-    def concat_contents(self, contents: list[CalcSizedContent]) -> list[str]:
+    def concat_contents(self) -> list[str]:
         """文字数もしくはトークン数が最大文字数および最大トークン数を超えないようにコンテンツの文字列を結合する"""
         total_token: int = 0
         total_char: int = 0
         buffer_content = ""
         connected_contents = []
 
-        for content in contents:
+        for content in self.calc_sized_contents:
             total_token += content.token
             total_char += content.char
             if self.max_token < total_token or self.max_char < total_char:
@@ -65,3 +74,17 @@ class ContentSizeOptimizer:
                 buffer_content += content.content
         connected_contents.append(buffer_content)
         return connected_contents
+
+    def calc_total_token(self) -> int:
+        """合計トークン数を計算する"""
+        total_token = 0
+        for content in self.calc_sized_contents:
+            total_token += content.token
+        return total_token
+
+    def calc_total_char(self) -> int:
+        """合計文字数を計算する"""
+        total_char = 0
+        for content in self.calc_sized_contents:
+            total_char += content.char
+        return total_char
