2f9506e69a40eca26f62322b81bb7e3b3b814b70
chemitaro <iwasi_44@hotmail.com>
2023-12-09 08:21:19 +0900

    Refactor URLTree class and add print_tree method

--- apps/lib/show_tree.py
+++ apps/lib/show_tree.py
@@ -2,35 +2,42 @@ from urllib.parse import urlparse
 from collections import defaultdict
 
 
-def parse_url(url):
-    parsed_url = urlparse(url)
-    paths = parsed_url.path.split('/')
-    return filter(None, paths)  # 空の要素を除去
+class URLTree:
+    tree: defaultdict
 
+    def __init__(self, urls):
+        self.tree = self.create_tree(urls)
 
-def insert_into_tree(tree, paths):
-    for path in paths:
-        if path not in tree:
-            tree[path] = defaultdict(dict)
-        tree = tree[path]
+    def parse_url(self, url):
+        parsed_url = urlparse(url)
+        paths = parsed_url.path.split('/')
+        return filter(None, paths)  # 空の要素を除去
 
+    def insert_into_tree(self, tree, paths):
+        for path in paths:
+            if path not in tree:
+                tree[path] = defaultdict(dict)
+            tree = tree[path]
 
-def create_tree(urls):
-    tree = defaultdict(dict)
-    for url in urls:
-        paths = list(parse_url(url))
-        insert_into_tree(tree, paths)
-    return tree
+    def create_tree(self, urls):
+        tree = defaultdict(dict)
+        for url in urls:
+            paths = list(self.parse_url(url))
+            self.insert_into_tree(tree, paths)
+        return tree
 
-
-def print_tree(tree, prefix="", is_root=True):
-    items = list(tree.items())
-    for i, (key, value) in enumerate(items):
-        connector = "" if is_root else ("└── " if i == len(items) - 1 else "├── ")
-        new_prefix = "" if is_root else (prefix + ("    " if i == len(items) - 1 else "│   "))
-        print(prefix + connector + key)
-        if isinstance(value, dict):
-            print_tree(value, new_prefix, is_root=False)
+    def print_tree(self, tree=None, prefix="", is_root=True):
+        if tree is None:
+            tree = self.tree
+        result = ""
+        items = list(tree.items())
+        for i, (key, value) in enumerate(items):
+            connector = "" if is_root else ("└── " if i == len(items) - 1 else "├── ")
+            new_prefix = "" if is_root else (prefix + ("    " if i == len(items) - 1 else "│   "))
+            result += prefix + connector + key + "\n"
+            if isinstance(value, dict):
+                result += self.print_tree(value, new_prefix, is_root=False)
+        return result
 
 
 # 使用例
@@ -45,5 +52,6 @@ urls = [
     'http://example.com/xyz/def/ghi'
 ]
 
-tree = create_tree(urls)
-print_tree(tree)
+url_tree = URLTree(urls)
+tree_string = url_tree.print_tree()
+print(tree_string)
