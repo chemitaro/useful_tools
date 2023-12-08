86273b1dadfcf70f98b85fe143b6fc892516bb7c
chemitaro <iwasi_44@hotmail.com>
2023-12-08 10:42:49 +0900

    Add Python file analysis test

--- tests/apps/lib/dependency_analyzer/test_main.py
+++ tests/apps/lib/dependency_analyzer/test_main.py
@@ -44,3 +44,23 @@ class TestDependencyAnalyzer:
         assert os.path.join(mock_path, 'ts_mock/ts_mock_a/ts_mock_a_b/ts_mock_a_b_1.ts') in result_paths
         assert os.path.join(mock_path, 'ts_mock/ts_mock_b/ts_mock_b_a/ts_mock_b_a_1.ts') in result_paths
         assert os.path.join(mock_path, 'ts_mock/ts_mock_b/ts_mock_b_b/ts_mock_b_b_1.ts') in result_paths
+
+    def test_py_analyze(self):
+        """Python のファイルを解析できることを確認する"""
+        # 解析する
+        analyzer = DependencyAnalyzer.factory(
+            mock_path,
+            ['py_mock/py_mock_1.py']
+        )
+        result_paths = analyzer.analyze()
+
+        # 解析結果を確認
+        assert type(result_paths) is list
+        assert len(result_paths) == 7
+        assert os.path.join(mock_path, 'py_mock/py_mock_1.py') in result_paths
+        assert os.path.join(mock_path, 'py_mock/py_mock_a/py_mock_a_1.py') in result_paths
+        assert os.path.join(mock_path, 'py_mock/py_mock_b/py_mock_b_1.py') in result_paths
+        assert os.path.join(mock_path, 'py_mock/py_mock_a/py_mock_a_a/py_mock_a_a_1.py') in result_paths
+        assert os.path.join(mock_path, 'py_mock/py_mock_a/py_mock_a_b/py_mock_a_b_1.py') in result_paths
+        assert os.path.join(mock_path, 'py_mock/py_mock_b/py_mock_b_a/py_mock_b_a_1.py') in result_paths
+        assert os.path.join(mock_path, 'py_mock/py_mock_b/py_mock_b_b/py_mock_b_b_1.py') in result_paths
