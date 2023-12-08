1eb9d891f380eca3177533e25db833d0210378e1
chemitaro <iwasi_44@hotmail.com>
2023-12-08 17:29:27 +0900

    add mock code

--- tests/mock/py_mock/py_mock_a/py_mock_a_b/py_mock_a_b_2.py
+++ tests/mock/py_mock/py_mock_a/py_mock_a_b/py_mock_a_b_2.py
@@ -1,1 +1,87 @@
 py_mock_a_b_2 = 'py_mock_a_b_2'
+
+# Define variables
+variable1 = "This"
+variable2 = "is"
+variable3 = "a"
+variable4 = "dummy"
+variable5 = "code"
+
+# Combine variables
+combined_text = " ".join([variable1, variable2, variable3, variable4, variable5])
+
+# Print the combined text
+print(combined_text)
+
+# Reverse the combined text
+reversed_text = combined_text[::-1]
+
+# Print the reversed text
+print(reversed_text)
+
+# Loop through each character in the combined text
+for character in combined_text:
+    print(character)
+
+# Convert the combined text to uppercase and print it
+print(combined_text.upper())
+
+# Check if "code" is in the combined text
+is_code = "code" in combined_text
+
+# Print the result
+print(f"Is 'code' in the text: {is_code}")
+
+# Define lists
+list1 = ["This", "is", "a", "dummy", "code"]
+list2 = [1, 2, 3, 4, 5]
+
+# Combine the lists
+combined_list = list1 + list2
+
+# Print the combined list
+print(combined_list)
+
+# Loop through the combined list and print its elements
+for element in combined_list:
+    print(element)
+
+# Sort the combined list and print it
+sorted_list = sorted(combined_list)
+print(sorted_list)
+
+# Reverse the sorted list and print it
+reversed_list = sorted_list[::-1]
+print(reversed_list)
+
+# Define strings
+string1 = "This is a dummy string."
+string2 = "This is another dummy string."
+
+# Concatenate the strings
+combined_string = string1 + " " + string2
+
+# Print the combined string
+print(combined_string)
+
+# Split the combined string into words and print them
+words = combined_string.split()
+print(words)
+
+# Count the number of words in the combined string
+word_count = len(words)
+
+# Print the word count
+print(f"Number of words: {word_count}")
+
+# Check if "dummy" is in the combined string
+is_dummy = "dummy" in combined_string
+
+# Print the result
+print(f"Is 'dummy' in the string: {is_dummy}")
+
+# Check if the two strings are equal
+strings_equal = string1 == string2
+
+# Print the result
+print(f"Are the strings equal: {strings_equal}")
--- tests/mock/ts_mock/ts_mock_a/ts_mock_a_a/ts_mock_a_a_2.ts
+++ tests/mock/ts_mock/ts_mock_a/ts_mock_a_a/ts_mock_a_a_2.ts
@@ -1,1 +1,61 @@
 export const ts_mock_a_a_2 = 'ts_mock_a_a_2';
+
+interface Point {
+  x: number;
+  y: number;
+}
+
+function generateRandomPoint(): Point {
+  return {
+    x: Math.random(),
+    y: Math.random(),
+  };
+}
+
+const points: Point[] = [];
+
+for (let i = 0; i < 5; i++) {
+  points.push(generateRandomPoint());
+}
+
+console.log(points);
+
+interface Shape {
+  name: string;
+  area(): number;
+}
+
+class Circle implements Shape {
+  radius: number;
+
+  constructor(radius: number) {
+    this.radius = radius;
+  }
+  name: string;
+
+  area(): number {
+    return Math.PI * this.radius * this.radius;
+  }
+}
+
+class Square implements Shape {
+  sideLength: number;
+
+  constructor(sideLength: number) {
+    this.sideLength = sideLength;
+  }
+  name: string;
+
+  area(): number {
+    return this.sideLength * this.sideLength;
+  }
+}
+
+const shapes: Shape[] = [];
+
+shapes.push(new Circle(5));
+shapes.push(new Square(10));
+
+for (const shape of shapes) {
+  console.log(`${shape.name}: ${shape.area()}`);
+}
