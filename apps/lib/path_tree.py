d1710c1415b57be356a95451b50f869866d9ae0d
chemitaro <iwasi_44@hotmail.com>
2023-12-09 14:58:03 +0900

    Add PathTree class and its tests

---
+++ apps/lib/path_tree.py
@@ -0,0 +1,70 @@
+from urllib.parse import urlparse
+from collections import defaultdict
+import os
+
+from apps.lib.utils import make_relative_path
+
+
+class PathTree:
+    paths: list[str]
+    root_path: str
+    tree: defaultdict
+
+    def __init__(self, paths, root_path=None):
+        self.root_path = root_path
+        self.paths = paths
+
+        # パスのリストから木構造を生成
+        self.tree = self.create_tree(self.paths)
+
+    def parse_url(self, url):
+        parsed_url = urlparse(url)
+        return filter(None, parsed_url.path.split('/'))  # 空の要素を除去
+
+    def parse_directory_path(self, path):
+        if self.root_path and path.startswith(self.root_path):
+            root_dir_path = os.path.dirname(self.root_path)
+            path = make_relative_path(root_dir_path, path)
+        return filter(None, path.split(os.sep))  # 空の要素を除去
+
+    def insert_into_tree(self, tree, paths):
+        for path in paths:
+            if path not in tree:
+                tree[path] = defaultdict(dict)
+            tree = tree[path]
+
+    def create_tree(self, paths):
+        tree = defaultdict(dict)
+        for path in paths:
+            if path.startswith('http://') or path.startswith('https://'):
+                parsed_paths = list(self.parse_url(path))
+            else:
+                parsed_paths = list(self.parse_directory_path(path))
+            self.insert_into_tree(tree, parsed_paths)
+        return tree
+
+    def print_tree(self, tree=None, prefix="", is_root=True):
+        if tree is None:
+            tree = self.tree
+        result = ""
+        items = list(tree.items())
+        for i, (key, value) in enumerate(items):
+            connector = "" if is_root else ("└── " if i == len(items) - 1 else "├── ")
+            new_prefix = "" if is_root else (prefix + ("    " if i == len(items) - 1 else "│   "))
+            result += prefix + connector + key + "\n"
+            if isinstance(value, dict):
+                result += self.print_tree(value, new_prefix, is_root=False)
+        return result
+
+
+# 使用例
+urls = [
+    'http://example.com/foo/bar',
+    'http://example.com/foo/baz',
+    'http://example.com/xyz',
+    'http://example.com/foo/bar/item1',
+    'http://example.com/foo/bar/item2',
+    'http://example.com/xyz/abc',
+    'http://example.com/xyz/def',
+    'http://example.com/xyz/def/ghi'
+]
---
+++ tests/apps/lib/test_path_tree.py
@@ -0,0 +1,52 @@
+from apps.lib.path_tree import PathTree
+
+
+class TestPathTree:
+    """PathTree のテスト"""
+
+    urls = [
+        'http://example.com/foo/bar',
+        'http://example.com/foo/baz',
+        'http://example.com/xyz',
+        'http://example.com/foo/bar/item1',
+        'http://example.com/foo/bar/item2',
+        'http://example.com/xyz/abc',
+        'http://example.com/xyz/def',
+        'http://example.com/xyz/def/ghi'
+    ]
+
+    paths = [
+        '/User/hoge/home/user/documents',
+        '/User/hoge/home/user/documents/file.txt',
+        '/User/hoge/home/user/pictures',
+        '/User/hoge/home/user/pictures/photo.jpg',
+        '/User/hoge/home/user/pictures/today/2021-01-01.jpg',
+        '/User/hoge/home/user/pictures/today/2021-01-02.jpg',
+        '/User/hoge/home/user/pictures/today/morning/2021-01-02.jpg',
+        '/User/hoge/home/user/pictures/old/2020-12-31.jpg',
+        '/User/hoge/home/user/pictures/old/2020-12-30.jpg',
+    ]
+
+    root_path = '/User/hoge/home/user'
+
+    def test_init(self):
+        """初期化してインスタンスを生成できることを確認する"""
+        path_tree = PathTree([])
+        assert type(path_tree) is PathTree
+
+    def test_view_tree_urls(self):
+        """URL のリストから木構造を生成できることを確認する"""
+        url_tree = PathTree(self.urls)
+        assert url_tree.print_tree() == 'foo\n├── bar\n│   ├── item1\n│   └── item2\n└── baz\nxyz\n├── abc\n└── def\n    └── ghi\n'
+
+    def test_view_tree_paths(self):
+        """パスのリストから木構造を生成できることを確認する"""
+        path_tree = PathTree(self.paths)
+        assert type(path_tree.print_tree()) == str
+        assert path_tree.print_tree() == 'User\n└── hoge\n    └── home\n        └── user\n            ├── documents\n            │   └── file.txt\n            └── pictures\n                ├── photo.jpg\n                ├── today\n                │   ├── 2021-01-01.jpg\n                │   ├── 2021-01-02.jpg\n                │   └── morning\n                │       └── 2021-01-02.jpg\n                └── old\n                    ├── 2020-12-31.jpg\n                    └── 2020-12-30.jpg\n'
+
+    def test_view_tree_paths_with_root(self):
+        """パスのリストから木構造を生成できることを確認する"""
+        path_tree = PathTree(self.paths, root_path=self.root_path)
+        assert type(path_tree.print_tree()) == str
+        assert path_tree.print_tree() == 'user\n├── documents\n│   └── file.txt\n└── pictures\n    ├── photo.jpg\n    ├── today\n    │   ├── 2021-01-01.jpg\n    │   ├── 2021-01-02.jpg\n    │   └── morning\n    │       └── 2021-01-02.jpg\n    └── old\n        ├── 2020-12-31.jpg\n        └── 2020-12-30.jpg\n'
