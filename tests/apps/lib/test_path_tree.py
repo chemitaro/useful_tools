248d9949b1b32a8ff723dca84729b73426bad3c5
chemitaro <iwasi_44@hotmail.com>
2023-12-09 15:10:08 +0900

    Refactor test_path_tree.py to improve readability and maintainability

--- tests/apps/lib/test_path_tree.py
+++ tests/apps/lib/test_path_tree.py
@@ -43,10 +43,10 @@ class TestPathTree:
         """パスのリストから木構造を生成できることを確認する"""
         path_tree = PathTree(self.paths)
         assert type(path_tree.print_tree()) == str
-        assert path_tree.print_tree() == 'User\n└── hoge\n    └── home\n        └── user\n            ├── documents\n            │   └── file.txt\n            └── pictures\n                ├── photo.jpg\n                ├── today\n                │   ├── 2021-01-01.jpg\n                │   ├── 2021-01-02.jpg\n                │   └── morning\n                │       └── 2021-01-02.jpg\n                └── old\n                    ├── 2020-12-31.jpg\n                    └── 2020-12-30.jpg\n'
+        assert path_tree.print_tree() == 'User\n└── hoge\n    └── home\n        └── user\n            ├── documents\n            │   └── file.txt\n            └── pictures\n                ├── photo.jpg\n                ├── today\n                │   ├── 2021-01-01.jpg\n                │   ├── 2021-01-02.jpg\n                │   └── morning\n                │       └── 2021-01-02.jpg\n                └── old\n                    ├── 2020-12-31.jpg\n                    └── 2020-12-30.jpg\n'  # noqa: E501
 
     def test_view_tree_paths_with_root(self):
         """パスのリストから木構造を生成できることを確認する"""
         path_tree = PathTree(self.paths, root_path=self.root_path)
         assert type(path_tree.print_tree()) == str
-        assert path_tree.print_tree() == 'user\n├── documents\n│   └── file.txt\n└── pictures\n    ├── photo.jpg\n    ├── today\n    │   ├── 2021-01-01.jpg\n    │   ├── 2021-01-02.jpg\n    │   └── morning\n    │       └── 2021-01-02.jpg\n    └── old\n        ├── 2020-12-31.jpg\n        └── 2020-12-30.jpg\n'
+        assert path_tree.print_tree() == 'user\n├── documents\n│   └── file.txt\n└── pictures\n    ├── photo.jpg\n    ├── today\n    │   ├── 2021-01-01.jpg\n    │   ├── 2021-01-02.jpg\n    │   └── morning\n    │       └── 2021-01-02.jpg\n    └── old\n        ├── 2020-12-31.jpg\n        └── 2020-12-30.jpg\n'  # noqa: E501
