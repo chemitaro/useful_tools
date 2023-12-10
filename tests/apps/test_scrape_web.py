0e6823c1926c9dee1a178736d966cdf2948c2e02
chemitaro <iwasi_44@hotmail.com>
2023-12-10 18:31:44 +0900

    Refactor test_main in test_scrape_web.py for improved readability and maintainability

--- tests/apps/test_scrape_web.py
+++ tests/apps/test_scrape_web.py
@@ -1,12 +1,22 @@
 from apps.scrape_web import main
+from apps.lib.utils import count_tokens
 
 
 class TestScrapeWeb:
-    url = 'https://www.nintendo.co.jp/n02/shvc/bm4j/'
+    url = 'https://nextjs.org/docs/app'
 
     def test_main(self):
         """メインの処理を実行できることを確認する"""
         # スクレイピングを実行する
-        contents = main([self.url])
+        contents = main(
+            [self.url],
+            limit_token=10000,
+            limit_char=10000
+        )
 
         assert type(contents) is list
+        assert len(contents) > 0
+        assert all([type(content) is str for content in contents])
+        assert all([len(content) > 0 for content in contents])
+        assert sum(count_tokens(content) for content in contents[1:]) < 10000
+        assert sum(len(content) for content in contents[1:]) < 10000
