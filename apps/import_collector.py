ada74d4d35ea8948ea9f24bfc64d7dab7d8f645d
chemitaro <iwasi_44@hotmail.com>
2023-12-13 13:47:29 +0900

    Refactor code to use dataclass and update command line arguments

--- apps/import_collector.py
+++ apps/import_collector.py
@@ -3,6 +3,7 @@
 import argparse
 import os
 import sys
+from dataclasses import dataclass
 
 # 現在のファイルの絶対パスを取得
 current_file_path = os.path.abspath(__file__)
@@ -20,17 +21,34 @@ from apps.lib.dependency_analyzer.main import DependencyAnalyzer  # noqa: E402
 from apps.lib.file_content_collector import FileContentCollector  # noqa: E402
 from apps.lib.outputs import copy_to_clipboard, print_result  # noqa: E402
 from apps.lib.path_tree import PathTree  # noqa: E402
+from apps.lib.utils import print_colored  # noqa: E402
+
+default_depth = 999
+default_max_char = 999_999_999
+default_max_token = 25_000
+
+
+@dataclass
+class MainArgs:
+    root_path: str
+    target_paths: list[str] | None
+    scope_paths: list[str] | None
+    ignore_paths: list[str] | None
+    depth: int | None
+    no_comment: bool | None
+    max_char: int | None
+    max_token: int | None
 
 
 def main(
     root_path: str,
     target_paths: list[str] | None = None,
-    depth: int = 999,
-    no_comment: bool = False,
-    max_char: int = 999_999_999,
-    max_token: int = 120_000,
     scope_paths: list[str] | None = None,
-    ignore_paths: list[str] | None = None
+    ignore_paths: list[str] | None = None,
+    depth: int = default_depth,
+    no_comment: bool = False,
+    max_char: int = default_max_char,
+    max_token: int = default_max_token,
 ) -> list[str]:
     """指定されたファイルのパスのファイルの内容を取得する
 
@@ -51,31 +69,6 @@ def main(
         scope_paths = []
     if ignore_paths is None:
         ignore_paths = []
-
-    # 引数の検証
-    if type(root_path) is not str:
-        raise TypeError('root_path must be str')
-    if type(target_paths) is not list:
-        raise TypeError('module_paths must be list')
-    if type(depth) is not int:
-        raise TypeError('depth must be int')
-    # depthは0以上の整数でなければならない
-    if depth < 0:
-        raise ValueError('depth must be positive')
-    if type(no_comment) is not bool:
-        raise TypeError('no_comment must be bool')
-    if type(max_char) is not int:
-        raise TypeError('max_chara must be int')
-    if max_char < 1:
-        raise ValueError('max_chara must be positive')
-    if type(max_token) is not int:
-        raise TypeError('max_token must be int')
-    if max_token < 1:
-        raise ValueError('max_token must be positive')
-    if type(scope_paths) is not list:
-        raise TypeError('scopes must be list')
-    if type(ignore_paths) is not list:
-        raise TypeError('ignores must be list')
 
     # ファイルの依存関係を解析
     dependency_analyzer = DependencyAnalyzer.factory(
@@ -125,14 +118,24 @@ if __name__ == "__main__":
     parser.add_argument(
         'target_path',
         nargs='*',
-        default=[],
         help='Path of the Python file from which to parse dependencies, multiple paths can be specified'
+    )
+    parser.add_argument(
+        '-s',
+        '--scope',
+        nargs='*',
+        help='Specify paths of files to scope, multiple files can be specified'
+    )
+    parser.add_argument(
+        '-i',
+        '--ignore',
+        nargs='*',
+        help='Specify paths of files to ignore, multiple files can be specified'
     )
     parser.add_argument(
         '-d',
         '--depth',
         type=int,
-        default=999,
         help='Specify depth of dependency analysis'
     )
     parser.add_argument(
@@ -142,51 +145,90 @@ if __name__ == "__main__":
         help='Omit document comments'
     )
     parser.add_argument(
-        '-mc',
-        '--max_char',
-        type=int,
-        default=999_999_999,
-        help='Split by a specified number of characters when copying to the clipboard'
-    )
-    parser.add_argument(
         '-mt',
         '--max_token',
         type=int,
-        default=120_000,
         help='Split by a specified number of tokens when copying to the clipboard'
     )
     parser.add_argument(
-        '-s',
-        '--scope',
-        nargs='*',
-        default=[],
-        help='Specify paths of files to scope, multiple files can be specified'
-    )
-    parser.add_argument(
-        '-i',
-        '--ignore',
-        nargs='*',
-        default=[],
-        help='Specify paths of files to ignore, multiple files can be specified'
+        '-mc',
+        '--max_char',
+        type=int,
+        help='Split by a specified number of characters when copying to the clipboard'
     )
     args = parser.parse_args()
 
-    root_dir = os.getcwd()
-
-    # メイン処理
-    chunked_content = main(
-        root_dir,
+    # コマンドライン引数をMainArgsに変換
+    main_args: MainArgs = MainArgs(
+        root_path=os.getcwd(),
         target_paths=args.target_path,
+        scope_paths=args.scope,
+        ignore_paths=args.ignore,
         depth=args.depth,
         no_comment=args.no_comment,
         max_char=args.max_char,
-        max_token=args.max_token,
-        scope_paths=args.scope,
-        ignore_paths=args.ignore
+        max_token=args.max_token
+    )
+
+    # 不足している引数がある場合は、input()で入力を求める
+    if not main_args.target_paths:
+        print_colored('\n依存関係を解析するファイルのパスを入力してください。', (" default: None", "grey"))
+        input_data = input('target_path: ')
+        if input_data:
+            main_args.target_paths = input_data.split(' ')
+
+    if not main_args.scope_paths:
+        print_colored('\n探索範囲のファイルのパスを入力してください。', (" default: None", "grey"))
+        input_data = input('scope_paths: ')
+        if input_data:
+            main_args.scope_paths = input_data.split(' ')
+
+    if not main_args.ignore_paths:
+        print_colored('\n無視するファイルのパスを入力してください。', (" default: None", "grey"))
+        input_data = input('ignore_paths: ')
+        if input_data:
+            main_args.ignore_paths = input_data.split(' ')
+
+    if main_args.target_paths and not main_args.depth:
+        print_colored('\n依存関係を解析する深さを入力してください。', (" default: 999", "grey"))
+        input_data = input('depth: ')
+        if input_data:
+            main_args.depth = int(input_data)
+
+    if not main_args.no_comment:
+        print_colored('\nコメントを除去しますか？("y" or "n")', (" default: n", "grey"))
+        input_data = input('no_comment: ')
+        if input_data == 'y':
+            main_args.no_comment = True
+        else:
+            main_args.no_comment = False
+
+    if not main_args.max_token:
+        print_colored('\n分割するトークン数を入力してください。', (" default: 25,000", "grey"))
+        input_data = input('max_token: ')
+        if input_data:
+            main_args.max_token = int(input_data)
+
+    if not main_args.max_char:
+        print_colored('\n分割する文字数を入力してください。', (" default: 999,999,999, Gemini: 30,000", "grey"))
+        input_data = input('max_char: ')
+        if input_data:
+            main_args.max_char = int(input_data)
+
+    # メイン処理
+    chunked_content = main(
+        main_args.root_path,
+        target_paths=main_args.target_paths,
+        scope_paths=main_args.scope_paths,
+        ignore_paths=main_args.ignore_paths,
+        depth=main_args.depth or default_depth,
+        no_comment=main_args.no_comment or False,
+        max_char=main_args.max_char or default_max_char,
+        max_token=main_args.max_token or default_max_token
     )
 
     # 取得したコードと文字数やトークン数、chunkの数を表示する
-    print_result(chunked_content, max_char=args.max_char, max_token=args.max_token)
+    print_result(chunked_content, max_char=main_args.max_char, max_token=main_args.max_token)
 
     # chunked_content を順番にクリップボードにコピーする
     copy_to_clipboard(chunked_content)
