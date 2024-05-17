c5d28789388dac0f3e8a3f50267c878827d3eeda
chemitaro <iwasi_44@hotmail.com>
2024-05-17 14:15:48 +0900

    クリップボードの設定とコピー機能を更新

--- apps/lib/clipboard_util.py
+++ apps/lib/clipboard_util.py
@@ -1,9 +1,44 @@
+import platform
+
 import pyperclip
 
 from apps.lib.utils import count_tokens, format_number, print_colored
 
 
-def copy_to_clipboard(contents: list[str] | str) -> None:
+def set_clipboard():
+    """
+    実行環境に応じて適切なクリップボード設定を適用する
+    """
+    system = platform.system()
+    if system == "Linux":
+        pyperclip.set_clipboard("xclip")
+    elif system == "Darwin":
+        pyperclip.set_clipboard("pbcopy")
+    elif system == "Windows":
+        pyperclip.set_clipboard("clip")
+    else:
+        raise RuntimeError(f"Unsupported platform: {system}")
+
+
+# クリップボード設定を適用
+set_clipboard()
+
+
+# 文字列をクリップボードにコピーする
+def copy_to_clipboard(content: str) -> None:
+    """
+    content をクリップボードにコピーする
+
+    Args:
+        content (str): コピーする内容
+
+    Returns:
+        None
+    """
+    pyperclip.copy(content)
+
+
+def copy_chunks_to_clipboard(chunk_contents: list[str] | str) -> None:
     """
     chunked_content を順番にクリップボードにコピーする
 
@@ -13,21 +48,21 @@ def copy_to_clipboard(contents: list[str] | str) -> None:
     Returns:
         None
     """
-    if isinstance(contents, str):
-        contents = [contents]
+    if isinstance(chunk_contents, str):
+        chunk_contents = [chunk_contents]
 
     print_colored(("\n== Copy to clipboard ==", "green"))
 
-    for content in contents:
-        pyperclip.copy(content)
+    for content in chunk_contents:
+        copy_to_clipboard(content)
         # chunkのナンバーを表示する
-        print_colored(f"\nChunk {contents.index(content) + 1} of {len(contents)} copied to clipboard.")
+        print_colored(f"\nChunk {chunk_contents.index(content) + 1} of {len(chunk_contents)} copied to clipboard.")
         # 文字数とトークン数を表示する
         total_char = len(content)
         total_tokens = count_tokens(content)
         print_colored(f"  ({format_number(total_char)} char, {format_number(total_tokens)} tokens)")
         # chunkが最後のchunkでない場合、Enterキーを押すまで待機する
-        if contents.index(content) + 1 < len(contents):
+        if chunk_contents.index(content) + 1 < len(chunk_contents):
             input("\nPress Enter to continue...")
 
 
@@ -36,4 +71,4 @@ def print_and_copy(text) -> None:
     print("\n")
     print(text)
     print_colored(("Copy to clipboard.", "grey"))
-    pyperclip.copy(text)
+    copy_to_clipboard(text)
